72b9b0d339db0c1f35646991f659d2028f2628d9
Alex Marmon <aemarmon@gmail.com>
2023-02-07 12:42:29 -0800

    bump

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
 import * as core from "@actions/core";
-// import * as github from "@actions/github";
+import * as github from "@actions/github";
 import axios from "axios";
+import { runTSC } from "./run-tsc";
 
 async function run(): Promise<void> {
-  // `who-to-greet` input defined in action metadata file
-  const userId = core.getInput("user-id");
+  const user_id = core.getInput("user-id");
   const token = core.getInput("token");
-  console.log(`Hello ${userId} ${token}!`);
+  console.log(github.context);
+  // const branch = core.getInput("base-branch");
+  // const errorsArray: string[] = [];
 
-  const response = await axios.get(
-    `https://gh-actions.vercel.app/api/hello?userId=${userId}&token=${token}`
-  );
+  // const response = await axios.get(
+  //   `https://gh-actions.vercel.app/api/hello?userId=${userId}&token=${token}`
+  // );
 
-  console.log(response);
+  // console.log(response);
 
-  core.setOutput("errors-list", response.data);
+  // core.setOutput("errors-list", response.data?.status);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2);
   // console.log(`The event payload: ${payload}`);
+
+  const errorsArray = await runTSC();
+  console.log(errorsArray);
+
+  // TODO: possibly swap with https://github.com/actions/toolkit/tree/main/packages/http-client
+  const response = await axios.post(
+    `https://gh-actions.vercel.app/api/typescript-errors`,
+    {
+      token,
+      user_id,
+      branch: "develop", // TODO: temp
+      errors: [
+        "pages/api/helloFriends.ts(36,18): error TS7006: Parameter 'thing' implicitly has an 'any' type.\n",
+      ],
+    }
+  );
+
+  // TODO: send errors list to api
+  // TODO: post new errors in github as comment
 }
 
 try {
---
+++ src/run-tsc.ts
@@ -0,0 +1,26 @@
+import subProcess from "child_process";
+
+export async function runTSC(): Promise<string[]> {
+  const errorsArray: string[] = [];
+  return new Promise((resolve, reject) => {
+    // TODO: possibly swap this with https://github.com/actions/toolkit/tree/main/packages/exec
+    const child = subProcess.spawn("yarn", ["tsc", "--noEmit"]); // TODO: verify the wd when this runs.
+
+    child.stdout.on("data", (data) => {
+      if (data.includes("error TS")) {
+        errorsArray.push(data?.toString());
+      }
+    });
+
+    child.stderr.on("data", (data) => {});
+
+    child.on("error", (error) => {
+      console.error(`error: ${error.message}`);
+      reject();
+    });
+
+    child.on("close", (code) => {
+      resolve(errorsArray);
+    });
+  });
+}
